8536a044fac4b1a96d564fd404aa514ac9dd83fe
MarianaOrsi <92837824+MarianaOrsi@users.noreply.github.com>
2021-11-20 19:03:24 -0300

    Add files via upload

---
+++ while.js
@@ -0,0 +1,15 @@
+let num = 1;
+//enquanto a variavel num for menor ou igual a 10, acrescente um numero
+while (num <= 10) {
+    console.log(num++)
+}
+
+//é executado 50 vezes, do 1 ao 50
+for (let cont = 1; cont <= 50; cont++) {
+    console.log(cont);
+}
+
+//é executado de traz para frente, do 10 ao 1
+for (cont = 10; cont > 0; cont--) {
+    console.log(cont)
+}
