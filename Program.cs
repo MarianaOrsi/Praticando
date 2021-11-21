ee65c1006cd848a82f695de51568e729848f2947
MarianaOrsi <92837824+MarianaOrsi@users.noreply.github.com>
2021-11-20 21:47:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Retangulo {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double area, perimetro, diagonal, base1, altura;
+
+            Console.Write("Base do retangulo: ");
+            base1 = double.Parse(Console.ReadLine(), CI);
+            Console.Write("Altura do retangulo: ");
+            altura = double.Parse(Console.ReadLine(), CI);
+
+            area = base1 * altura;
+            perimetro = 2 * (base1 + altura);
+            diagonal = Math.Sqrt(Math.Pow(base1, 2.0) + Math.Pow(altura, 2.0));
+
+            Console.WriteLine("AREA = " + area.ToString("F4", CI));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", CI));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CI));
+
+
+        }
+    }
+}
