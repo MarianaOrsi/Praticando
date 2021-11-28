107957103c4a5519bae8d251995c457b25d42dd5
MarianaOrsi <92837824+MarianaOrsi@users.noreply.github.com>
2021-11-28 20:39:17 -0300

    Add files via upload

---
+++ juntando-turmas.js
@@ -0,0 +1,6 @@
+const salaDePython = ["Melissa", "Helena", "Rodrigo", "Andre"];
+const salaDeJavaScript = ["Juliana", "Leonardo", "Raquel", "Bruna"];
+
+const salasUnificadas = salaDePython.concat(salaDeJavaScript);
+
+console.log(salasUnificadas);
