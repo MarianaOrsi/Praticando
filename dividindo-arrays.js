af44d5a5b03c7b2adf7d60ca35a5eeadd7e22235
MarianaOrsi <92837824+MarianaOrsi@users.noreply.github.com>
2021-11-28 20:15:15 -0300

    Update dividindo-arrays.js

--- dividindo-arrays.js
+++ dividindo-arrays.js
@@ -3,6 +3,6 @@ const nomes = ["João", "Juliana", "Ana", "Caio", "Lara", "Marjorie", "Guilherme
 const sala1 = nomes.slice(0, nomes.length/2);
 const sala2 = nomes.slice(nomes.length/2);
 
+console.log("Quantidade de alunos:", nomes.length);
 console.log(`Alunos da sala 1: ${sala1}\n`);
-
 console.log(`Alunos da sala 2: ${sala2}`);
